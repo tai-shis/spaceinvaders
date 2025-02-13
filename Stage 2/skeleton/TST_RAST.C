b020b68368aaa320d7dd94f6c17e4d50d497c2ff
Harinder <harinderspunia@gmail.com>
2025-02-13 09:47:59 -0700

    added plotting for other bitmaps in tst_rast

--- Stage 2/skeleton/TST_RAST.C
+++ Stage 2/skeleton/TST_RAST.C
@@ -10,6 +10,10 @@ int main()
 	plot_vline(base, 319, 0, 400);
 	
 	plot_bitmap32(base, 320, 200, player_bitmap, 32);
+	plot_bitmap32(base, 320, 232, alien1_bitmap_1, 32);
+	plot_bitmap32(base, 320, 264, alien2_bitmap_1, 32);
+	plot_bitmap32(base, 320, 296, alien3_bitmap_1, 32);
+	plot_bitmap32(base, 320, 328, mothership, 32);
 
 	return 0;
 }
