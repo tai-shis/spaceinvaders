c16b0240052e16196dbd631985240c3e49c9e05e
tai-shis <tshis070@mtroyal.ca>
2025-02-19 14:59:48 -0700

    Started stage 3 event methods
    Co-authored-by: hpunia <hpunia@users.noreply.github.com>

--- Stage 3/model.c
+++ Stage 3/model.c
@@ -1,2 +1,36 @@
 #include "model.h"
 
+void move_player(Model *model, Player *player) {
+    if(player->x <= model->lowbound_x) {
+        player->x = model->lowbound_x;
+    } else if (player->x >= model->highbound_x) {
+        player->x = model->highbound_x;
+    } else {
+        player->x += player->direction;
+    }
+}
+
+
+
+void alien_direc_change(Aliens *alien_arr) {
+    if (alien_arr->direction > 0) {
+        alien_arr->direction = -1;
+    }
+    else {
+        alien_arr->direction = 1;
+    }
+
+    return;
+}
+
+int move_bullet(Model *model, Bullet *bullet) {
+    /* If returns -1, event handlers should remove this object instance */
+    if (bullet->y <= model->lowbound_y) {
+        return -1;
+    } else if (bullet->y >= model->highbound_y) {
+        return -1;
+    } else {
+        bullet->y += bullet->y_direction;
+        return 0;
+    }
+}
--- Stage 3/model.h
+++ Stage 3/model.h
@@ -11,16 +11,19 @@ typedef struct {
     const int bullet_type;  /* Will be 1 */
 } Player;
 
+void move_player(Player *player);
+
 typedef struct {
-    UINT16 x, y;
+    UINT16 x, y;            /* Position Coordinates */
     UINT8 l, w;             /* Size of hitbox for collision detection */
-    int direction;          /* -1, 0, or 1 depending on direction of movement */
     int score;              /* Score awarded to Player */
-    int alive;              /* To be used as a boolean marker 0 = true, -1 = false */
-    const int bullet_type;  /* Will be -1 */
-    
+    int alive;              /* To be used as a boolean marker 0 = true, -1 = false */ 
     /* Add bullet fire timing somewhere */
 } Alien;
+
+void alien_direc_change(Aliens *alien_arr);
+
+void alien_level(Aliens *alien_arr);
 
 typedef struct {
     Alien r1[11];
@@ -28,7 +31,10 @@ typedef struct {
     Alien r3[11];
     Alien r4[11];
     Alien r5[11];
+    
     UINT8 totalAliens;
+    int direction;          /* -1 or 1 depending on direction of movement */
+    const int bullet_type;  /* Will be -1 */
 } Aliens;
 
 typedef struct {
@@ -36,6 +42,9 @@ typedef struct {
     UINT8 l, w;         /* Hitbox */
     int y_direction;    /* -1 or 1, moving down or up respectively */
 } Bullet;
+
+int move_bullet(Model *model, Bullet *bullet);
+}
 
 typedef struct {
     UINT16 score;       /* Score number */
@@ -46,6 +55,7 @@ typedef struct {
     Player player;
     Aliens aliens;
     Score score;
+    UINT16 lowbound_x, lowbound_y, highbound_x, highbound_y; /* Bounds of play area */
 } Model;
 
 #endif
