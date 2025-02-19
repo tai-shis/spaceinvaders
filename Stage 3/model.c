8cf97e0b7b18eb04f4760a6242c8f1720e34997b
hpunia <harinderspunia@gmail.com>
2025-02-19 16:43:49 -0700

    working on stage 3 events
    Co-authored-by: tai-shis <tai-shis@users.noreply.github.com>

--- Stage 3/model.c
+++ Stage 3/model.c
@@ -1,36 +1,100 @@
 #include "model.h"
 
 void move_player(Model *model, Player *player) {
+    /* Should be called every tick/check whenever the player's directional value is not 0 */
+    player->x += player->delta_x;
+
     if(player->x <= model->lowbound_x) {
         player->x = model->lowbound_x;
     } else if (player->x >= model->highbound_x) {
         player->x = model->highbound_x;
     } else {
-        player->x += player->direction;
+        return;
     }
 }
 
-
-
-void alien_direc_change(Aliens *alien_arr) {
-    if (alien_arr->direction > 0) {
-        alien_arr->direction = -1;
+void alien_direc_change(Model *model, Aliens *alien_arr) {
+    /* Changes direction of movement (left or right), called when aliens are lowered */
+    if (alien_arr->delta_x > 0) {
+        alien_arr->delta_x = -model->alien_delta_x;
     }
     else {
-        alien_arr->direction = 1;
+        alien_arr->delta_x = model->alien_delta_x;
     }
-
     return;
 }
 
+int move_aliens(Model *model, Aliens *aliens) {
+    /* Should be called every time that aliens are set to move, handles lowering aliens when necessary */
+    /* Assumes that the losing condition is reliant on the lowest possible position that an alien can be at*/
+    /* Returns -1 is aliens hits lower bound*/
+
+    /* Uses return value of lower_aliens() to check if game ends */
+    /* Also checks if next step will be out of bounds, and if so it lowers aliens instead */
+    if ((aliens->r1[0].x + aliens->delta_x) <= model->lowbound_x) {
+        return lower_aliens(model, aliens);
+    } else if ((aliens->r1[0].x + aliens->delta_x) >= model->highbound_x) {
+        return lower_aliens(model, aliens);
+    } else {
+        int i;
+        for (i = 0; i < 11; i++) {
+            aliens->r1[i].x += aliens->delta_x;
+            aliens->r2[i].x += aliens->delta_x;
+            aliens->r3[i].x += aliens->delta_x;
+            aliens->r4[i].x += aliens->delta_x;
+            aliens->r5[i].x += aliens->delta_x;
+        }
+        return 0;
+    }
+}
+
+int lower_aliens(Model *model, Aliens *alien_arr) {
+    /* Called when aliens x position meets edge, edge detection happens in move_aliens */
+    /* Also changes direction of travel */
+    int i;
+    for (i = 0; i < 11; i++) {
+        alien_arr->r1[i].y -= 32;
+        alien_arr->r2[i].y -= 32;
+        alien_arr->r3[i].y -= 32;
+        alien_arr->r4[i].y -= 32;
+        alien_arr->r5[i].y -= 32;
+    }
+    alien_direc_change(model, alien_arr);    
+
+    if(alien_arr->r5[0].y >= model->lowbound_y) {
+        return -1; /* Game over */
+    } else {
+        return 0;
+    }
+}
+
+void destroy_alien(Alien alien, Score curr_score) {
+    /* Called when bullet hitbox hits alien hitbox */
+    /* Total alien count changed from collision handling point */
+    /* Also adds score from destroying alien */
+    add_score(alien.score, curr_score);
+    alien.alive = -1;
+}
+
 int move_bullet(Model *model, Bullet *bullet) {
+    /* Should be called every tick to handle the bullet's movement */
     /* If returns -1, event handlers should remove this object instance */
+    bullet->y += bullet->y_direction;
+
     if (bullet->y <= model->lowbound_y) {
         return -1;
     } else if (bullet->y >= model->highbound_y) {
         return -1;
     } else {
-        bullet->y += bullet->y_direction;
         return 0;
     }
+}
+
+void add_score (int score_to_add, Score curr_score) {
+    /* Adds score from destorying alien */
+    curr_score.score += score_to_add;
+}
+
+void update_score(Score *score) {
+    /* redisplay score */
 }
--- Stage 3/model.h
+++ Stage 3/model.h
@@ -7,7 +7,7 @@ typedef struct {
     UINT16 x, y;            /* Position Coordinates, y should be constant, set in the constructor */
     UINT8 l, w;             /* Size of hitbox for collsion detection  */
     UINT8 lives;            /* How many lives this player has */
-    int direction;          /* Signed integer, -1, 0, or 1 depending on movement */
+    int delta_x;          /* Signed integer, -1, 0, or 1 depending on movement */
     const int bullet_type;  /* Will be 1 */
 } Player;
 
@@ -15,15 +15,10 @@ void move_player(Player *player);
 
 typedef struct {
     UINT16 x, y;            /* Position Coordinates */
-    UINT8 l, w;             /* Size of hitbox for collision detection */
     int score;              /* Score awarded to Player */
     int alive;              /* To be used as a boolean marker 0 = true, -1 = false */ 
     /* Add bullet fire timing somewhere */
 } Alien;
-
-void alien_direc_change(Aliens *alien_arr);
-
-void alien_level(Aliens *alien_arr);
 
 typedef struct {
     Alien r1[11];
@@ -31,11 +26,18 @@ typedef struct {
     Alien r3[11];
     Alien r4[11];
     Alien r5[11];
-    
     UINT8 totalAliens;
-    int direction;          /* -1 or 1 depending on direction of movement */
+    int delta_x;          /* -1(left) or 1(right) depending on direction of movement */
     const int bullet_type;  /* Will be -1 */
 } Aliens;
+
+void alien_direc_change(Model *model, Aliens *alien_arr);
+
+int move_aliens(Model *model, Aliens *aliens);
+
+int lower_aliens(Model *model, Aliens *alien_arr);
+
+void destroy_alien(Alien alien, Score curr_score);
 
 typedef struct {
     UINT16 x, y;        /* x coordinate should be set and not changed when the object is created */
@@ -44,18 +46,24 @@ typedef struct {
 } Bullet;
 
 int move_bullet(Model *model, Bullet *bullet);
-}
 
 typedef struct {
     UINT16 score;       /* Score number */
     UINT16 x, y;        /* Position of score sign */
 } Score;
 
+void add_score (int score_to_add, Score curr_score);
+
+void update_score(Score *score);
+
 typedef struct {
     Player player;
     Aliens aliens;
     Score score;
-    UINT16 lowbound_x, lowbound_y, highbound_x, highbound_y; /* Bounds of play area */
+    UINT16 lowbound_x, lowbound_y, highbound_x, highbound_y; /* Bounds of play area. highbounds should be bound - alien_w */ 
+    UINT8 alien_l, alien_w;    /* Used for movement and collision detection */      
+    int alien_delta_x;
+    int bullet_delta_y;
 } Model;
 
 #endif
